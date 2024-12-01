34ca5a4a78e6dd344f236260473fb1c644d5a4b6
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-12-01 13:34:26 +0330

    end Tic Tac Toe game project

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+#
+# number = {
+#     'reazi': 10,
+#     'farsi': 15,
+#     'varzesh': 17,
+#     'c': 5,
+# }
+#
+# sort = {k: sorted(v) for k, v in number.items()}
+#
+#
+# for (i, j) in number.items():
+#     value = number.values()
+#     key = number.keys()
 
-number = {
-    'reazi': 10,
-    'farsi': 15,
-    'varzesh': 17,
-    'c': 5,
-}
-
-sort = {k: sorted(v) for k, v in number.items()}
 
 
-for (i, j) in number.items():
-    value = number.values()
-    key = number.keys()
+def zarb():
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print(i * j, end=', ')
+        print()
 
-
-
+zarb()
--- tic-tac-toe-game/tic-tac-toe.py
+++ tic-tac-toe-game/tic-tac-toe.py
@@ -2,6 +2,7 @@ from termcolor import colored
 
 board = list(range(1, 10))
 winners = ((0, 1, 2,), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+moves = ((1, 3, 7, 9), (5,), (2, 4, 6, 8))
 
 
 def print_board():
@@ -12,7 +13,7 @@ def print_board():
             end = '\n'
         if i == 'X':
             print(colored(f'[{i}]', 'red'), end=end)
-        if i == 'O':
+        elif i == 'O':
             print(colored(f'[{i}]', 'blue'), end=end)
         else:
             print(f'[{i}]', end=end)
@@ -52,6 +53,34 @@ def has_empty_space():
     return board.count('X') + board.count('O') != 9
 
 
+def computer_move():
+    mv = -1
+    # computer can win ?
+    for i in range(1, 10):
+        if make_move(board, computer, i, True)[1]:
+            mv = i
+            break
+
+    # Player can win ?
+    if mv == -1:
+        for i in range(1, 10):
+            if make_move(board, player, i, True)[1]:
+                mv = i
+                break
+
+    # mov
+    if mv == -1:
+        for tup in moves:
+            for i in tup:
+                if can_move(board, i):
+                    mv = i
+                    break
+            if mv != -1:
+                break
+
+    return make_move(board, computer, mv)
+
+
 player, computer = 'X', 'O'
 print('Player: [X]\nComputer: [O]\n')
 
@@ -64,6 +93,9 @@ while has_empty_space():
         continue
     if won:
         print(colored('You are Won', 'green'))
+        break
+    elif computer_move()[1]:
+        print(colored('You are lose!', 'yellow'))
         break
 
 print_board()
