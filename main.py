38a6f7749f30d35f89605669e1baa164949cfd82
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-12-09 23:47:23 +0330

    .

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-s = 'abc'
-s *= 2
-s = s.replace('c', 'x')
-print(s)
+import time
+
+
+# s = 'abc'
+# s *= 2
+# s = s.replace('c', 'x')
+# print(s)
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a)
+        a, b = b, a + b
+
+
+def countdown(n):
+    while n >= 0:
+        mins, secs = divmod(n, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end='\r')
+        time.sleep(1)
+        n -= 1
+    print('lift out!')
+
+
+if __name__ == '__main__':
+    countdown(10)
